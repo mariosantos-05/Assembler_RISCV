96b924ff0d32f2f6370ad9d2936178027f43da4d
mariosantos-05 <marioaugustov.s@gmail.com>
2026-04-17 21:56:58 -0300

    Feat: add assembler

--- assembler.py
+++ assembler.py
@@ -1,6 +1,6 @@
 def read_file(path):
     with open(path, 'r') as f:
-        return f.read()
+        return f.readlines()
     
 
 ''' 
@@ -9,6 +9,7 @@ parser course, i would have 2 coins. what is not much but is odd
 
 '''
 def parse_line(line):
+    line = line.split('#')[0]  # remove inline comments
     line = line.strip() # get rid of leading and trailing whitespace
 
     if not line or line.startswith('#'):  # get out of here stink comments. we assemblers do not care
@@ -20,8 +21,6 @@ def parse_line(line):
     return parts
 
 
-
-
 # Formato R [ funct7 ][ rs2 ][ rs1 ][ funct3 ][ rd ][ opcode ]
 def encode_add(rd, rs1, rs2):
 
@@ -29,20 +28,16 @@ def encode_add(rd, rs1, rs2):
     funct3 = "000"
     funct7 = "0000000"
 
-    rd_bin  = format(int(rd[1:]), '05b')
+    rd_bin  = format(int(rd[1:]), '05b')  # 32 bits, 5 bits for the register number (x0-x31)
     rs1_bin = format(int(rs1[1:]), '05b')
     rs2_bin = format(int(rs2[1:]), '05b')
 
     binary = funct7 + rs2_bin + rs1_bin + funct3 + rd_bin + opcode
-    hex_val = format(int(binary, 2), '08X')  # 8 dígitos hex
-
-    return hex_val
+    return format(int(binary, 2), '08X')  # 8 dígitos hex
 
 
 # mif = Memory Initialization File
 # it must be in the format of an hexadecimal
-
-
 
 def write_mif(hexadecimal_code, output_path='output.mif'):
     with open(output_path, 'w') as f:
@@ -61,4 +56,33 @@ def write_mif(hexadecimal_code, output_path='output.mif'):
         for i, code in enumerate(hexadecimal_code):
             f.write(f"{i:02X} : {code};\n") # write the address and the data in hexadecimal format
 
+        f.write("END;\n") #end of memory data
 
+
+def main():
+    lines = read_file('input.asm')
+
+    hexadecimal_code = []
+
+    for line in lines:
+        parsed = parse_line(line)
+        if parsed is None:
+            continue 
+
+        if parsed[0] == "add":
+            if len(parsed) != 4:
+                print(f"Erro de formato: {parsed}")
+                continue
+
+            _, rd, rs1, rs2 = parsed
+            hexadecimal_code.append(encode_add(rd, rs1, rs2)) 
+
+        else:
+            print(f"Invalid instruction: {parsed[0]}")
+
+    write_mif(hexadecimal_code)
+    print("Output written to output.mif")
+
+
+if __name__ == "__main__":
+    main()
---
+++ input.asm
@@ -0,0 +1,6 @@
+# test input
+
+add x1, x2, x3
+add x4, x5, x6  # inline comment
+
+sub x10, x11, x12 # testing a invalid instruction
